3d211c65779adf8b809b14f0b85a2ac97158b2f9
Jake Watkins <76190146+Watkin81@users.noreply.github.com>
2024-02-19 13:32:15 -0500

    Clients no longer enable functionality, creators do

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
+// v1.3.0
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
 #include <Geode/binding/GameStatsManager.hpp>
 #include <Geode/binding/GJGameLevel.hpp>
-#include <Geode/modify/GameLevelOptionsLayer.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/binding/PlayLayer.hpp>
+#include <Geode/modify/LevelOptionsLayer.hpp>
+#include <Geode/binding/TextGameObject.hpp>
+#include <Geode/binding/GameObject.hpp>
+#include <Geode/binding/GJBaseGameLayer.hpp>
+#include <Geode/modify/LevelEditorLayer.hpp>
 #include <chrono>
 #include <ctime>
 #include <random>
@@ -14,17 +20,132 @@ using namespace geode::prelude;
 int currentTime_t = 0;
 int currentTime_unix = 0;
 bool is_enabled = false;
+bool uses_unix = false;
 //int timeZoneOffset = -5;
 int lastUnix = 99999999999;
+GameObject* object;
+
+bool checkForEditorText() {
+    for (auto* obj: CCArrayExt<GameObject*>(GameManager::sharedState()->getEditorLayer()->m_objects)) {
+        if (obj->m_objectID == 914) {
+            auto* text = static_cast<TextGameObject*>(obj);
+            if (text && text->m_text == std::string_view("~!ThisTextEnablesUnix!~")) {
+                //log::info("checkforeditortext found object: {}", obj);
+                object = obj;
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+CCSprite* testEnabled(CCSprite* toggleOn, CCSprite* toggleOff) {
+    return (is_enabled) ? toggleOn : toggleOff;
+};
+
+class $modify(UnixTimeSwap, LevelOptionsLayer) {
+
+    void setupOptions() {
+        LevelOptionsLayer::setupOptions();
+        CCMenu* menu = CCMenu::create();
+        auto toggleOn = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
+        auto toggleOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+        CCMenuItemToggler* btn = CCMenuItemToggler::create(testEnabled(toggleOn, toggleOff), testEnabled(toggleOff, toggleOn), this, menu_selector(UnixTimeSwap::enable_unix));
+        
+        if (checkForEditorText() && !is_enabled) {
+            btn->activate();
+        }
+        else if (!checkForEditorText() && is_enabled) {
+            btn->activate();
+        }
+
+        CCLabelBMFont* label = CCLabelBMFont::create("Unix Time", "bigFont.fnt");
+
+        //btn->setPosition({316,182.8}); sorry Xanii D:
+        btn->setPosition({125,80});
+        btn->setScale(0.82);
+
+        label->setPosition({btn->getPositionX() + 54, btn->getPositionY()});
+        label->setScale(0.4);
+        addChild(label);
+
+        menu->setPosition({0,0});
+        menu->addChild(btn);
+        addChild(menu);
+        handleTouchPriority(this);
+    }
+
+    void enable_unix(CCObject*) {
+        is_enabled = !is_enabled;
+        if (is_enabled) {
+            object = GameManager::sharedState()->getEditorLayer()->createObject(914, ccp(-500000, 500000), true);
+            TextGameObject* textObject = static_cast<TextGameObject*>(object);
+            textObject->m_text = std::string("~!ThisTextEnablesUnix!~");
+        }
+        else if (!is_enabled) {
+            for (auto* obj: CCArrayExt<GameObject*>(GameManager::sharedState()->getEditorLayer()->m_objects)) {
+                if (obj->m_objectID == 914) {
+                    auto* text = static_cast<TextGameObject*>(obj);
+                    if (text && text->m_text == std::string_view("~!ThisTextEnablesUnix!~")) {
+                        log::info("found UNIX text object, deleting the object on this level!");
+                        //log::info("object is: {}", obj);
+                        GameManager::sharedState()->getEditorLayer()->removeObject(obj, true);
+                    }
+                }
+            }        
+        }
+    }
+};
+
+class $modify(PlayLayer) {
+    bool init(GJGameLevel* p0, bool p1, bool p2) {
+        if (!PlayLayer::init(p0,p1,p2)) {
+            return false;
+        }
+
+        uses_unix = false;
+        for (auto* obj: CCArrayExt<GameObject*>(GameManager::sharedState()->getPlayLayer()->m_objects)) {
+            if (obj->m_objectID == 914) {
+                auto* text = static_cast<TextGameObject*>(obj);
+                if (text && text->m_text == std::string_view("~!ThisTextEnablesUnix!~")) {
+                    log::info("found UNIX text object, enabling UnixSync functionality on this level!");
+                    uses_unix = true;
+                }
+            }
+        }
+
+        if (uses_unix) {
+            auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+            auto enabledDisplay = CCLabelBMFont::create("<text here>", "bigFont.fnt");
+            if (Mod::get()->getSettingValue<bool>("showunix")) {
+                enabledDisplay->setPosition(60, 5);
+            }
+            else {
+                enabledDisplay->setPosition(19, 5);
+            }
+            enabledDisplay->setScale(0.25);
+            enabledDisplay->setOpacity(127);
+            auto node = CCNode::create();
+            node->setZOrder(10);
+            node->addChild(enabledDisplay);
+            node->setID("unixtimestamp");
+            this->addChild(node);
+        }
+        return true;
+    }
+};
 
 class $modify(MyBaseGameLayer, GJBaseGameLayer) {
     void update(float delta) override {
         GJBaseGameLayer::update(delta);
-        if (is_enabled) {
+
+        //log::info("uses: {}", uses_unix);
+        if (uses_unix) {
             if (currentTime_unix != lastUnix) { // run once a second
                 oncePerSecond(1.f);
             }
-
+ 
             auto currentTime = std::chrono::system_clock::now();
             currentTime_t = std::chrono::system_clock::to_time_t(currentTime);
             currentTime_unix = currentTime_t;
@@ -98,59 +219,4 @@ class $modify(MyBaseGameLayer, GJBaseGameLayer) {
     }
 };
 
-CCSprite* testEnabled(CCSprite* toggleOn, CCSprite* toggleOff) {
-    return (is_enabled) ? toggleOn : toggleOff;
-}
-
-class $modify(UnixTimeSwap, GameLevelOptionsLayer) {
-    void setupOptions() {
-        GameLevelOptionsLayer::setupOptions();
-        CCMenu* menu = CCMenu::create();
-        auto toggleOn = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
-        auto toggleOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
-        CCMenuItemToggler* btn = CCMenuItemToggler::create(testEnabled(toggleOn, toggleOff), testEnabled(toggleOff, toggleOn), this, menu_selector(UnixTimeSwap::enable_unix));
-        CCLabelBMFont* label = CCLabelBMFont::create("Unix Time", "bigFont.fnt");
-
-        btn->setPosition({316,182.8});
-        btn->setScale(0.82);
-
-        label->setPosition({btn->getPositionX() + 56, btn->getPositionY()});
-        label->setScale(0.4);
-        addChild(label);
-
-        menu->setPosition({0,0});
-        menu->addChild(btn);
-        addChild(menu);
-        handleTouchPriority(this);
-    }
-
-    void enable_unix(CCObject*) {
-        is_enabled = !is_enabled;
-        log::info("UnixSync Status: {}", is_enabled);
-    }
-};
-
-class $modify(PlayLayer) {
-    bool init(GJGameLevel* p0, bool p1, bool p2) {
-        if (!PlayLayer::init(p0,p1,p2)) return false;
-        if (is_enabled) {
-            auto winSize = CCDirector::sharedDirector()->getWinSize();
-
-            auto enabledDisplay = CCLabelBMFont::create("<text here>", "bigFont.fnt");
-            if (Mod::get()->getSettingValue<bool>("showunix")) {
-                enabledDisplay->setPosition(60, 5);
-            }
-            else {
-                enabledDisplay->setPosition(19, 5);
-            }
-            enabledDisplay->setScale(0.25);
-            enabledDisplay->setOpacity(127);
-            auto node = CCNode::create();
-            node->setZOrder(10);
-            node->addChild(enabledDisplay);
-            node->setID("unixtimestamp");
-            this->addChild(node);
-        }
-        return true;
-    }
-};
+// gaming
